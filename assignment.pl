54d3d4fb7ffa7582025dae38e65f69c377573be8
Michalis Chatzispyrou <sdi2000212@di.uoa.gr>
2023-03-25 12:32:16 +0200

    first commit

---
+++ assignment.pl
@@ -0,0 +1,14 @@
+:- compile(activity).
+
+% ASP lista me stoixeia
+% N-As-T 
+
+% [ Atomo - [lista drastiriotion] - Sinolikos Xronos ]
+
+
+% ASA lista me stoixeia
+% A-N
+
+% [ Drastiriotika - Atomo ]
+
+% assignment(NP, MT, ASP, ASA).
